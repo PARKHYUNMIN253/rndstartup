5d6d618041b3ca091debcd74ea78e593c017a98c
Park Hyun-min(박현민) <hmparki253@naver.com>
2016-10-17 13:42:45 +0900

    initial commit

---
+++ BizOneShot.Light.Dao/Repositories/ScFileInfoRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using BizOneShot.Light.Dao.Infrastructure;
+using BizOneShot.Light.Models.WebModels;
+using PagedList;
+using PagedList.EntityFramework;
+
+
+namespace BizOneShot.Light.Dao.Repositories
+{
+    public interface IScFileInfoRepository : IRepository<ScFileInfo>
+    {
+        Task<ScFileInfo> getFileInfoByFileSn(int fileSn);
+        ScFileInfo getFileInfoByFileSnNA(int fileSn);
+    }
+
+    public class ScFileInfoRepository : RepositoryBase<ScFileInfo>, IScFileInfoRepository
+    {
+        public ScFileInfoRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+
+        public async Task<ScFileInfo> getFileInfoByFileSn(int fileSn)
+        {
+            return await DbContext.ScFileInfoes.Where(obj => obj.FileSn == fileSn).SingleAsync();
+        }
+
+        public ScFileInfo getFileInfoByFileSnNA(int fileSn)
+        {
+            return DbContext.ScFileInfoes.Where(obj => obj.FileSn == fileSn).Single();
+        }
+    }
+}
---
+++ BizOneShot.Light.Services/ScFileInfoService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using BizOneShot.Light.Dao.Infrastructure;
+using BizOneShot.Light.Dao.Repositories;
+using BizOneShot.Light.Models.WebModels;
+
+namespace BizOneShot.Light.Services
+{
+    public interface IScFileInfoService : IBaseService
+    {
+        void ModifyFileInfo(ScFileInfo scFileInfo);
+        Task<ScFileInfo> getFileInfoByFileSn(int fileSn);
+        ScFileInfo getFileInfoByFileSnNA(int fileSn);
+    }
+
+
+    public class ScFileInfoService : IScFileInfoService
+    {
+        private readonly IScFileInfoRepository scFileInfoRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ScFileInfoService(IScFileInfoRepository scFileInfoRepository, IUnitOfWork unitOfWork)
+        {
+            this.scFileInfoRepository = scFileInfoRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<ScFileInfo> getFileInfoByFileSn(int fileSn)
+        {
+            return await scFileInfoRepository.getFileInfoByFileSn(fileSn);
+        }
+
+        public ScFileInfo getFileInfoByFileSnNA(int fileSn)
+        {
+            return scFileInfoRepository.getFileInfoByFileSnNA(fileSn);
+        }
+
+        public void ModifyFileInfo(ScFileInfo scFileInfo)
+        {
+            scFileInfoRepository.Update(scFileInfo);
+        }
+
+        #region SaveDbContext
+
+        public void SaveDbContext()
+        {
+            unitOfWork.Commit();
+        }
+
+        public async Task<int> SaveDbContextAsync()
+        {
+            return await unitOfWork.CommitAsync();
+        }
+
+        
+
+        #endregion
+    }
+}
